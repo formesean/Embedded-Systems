2ae8cf205d7d257ec7b900f1a57b7d5f3beb2155
Sean Aguilar <seanaguilar698@gmail.com>
2025-02-10 14:52:57 +0800

    add configuration bits

---
+++ configurationBits.c
@@ -0,0 +1,10 @@
+#include<xc.h> // include file for the XC8 compiler
+
+#pragma config FOSC = XT
+#pragma config WDTE = OFF
+#pragma config PWRTE = ON
+#pragma config BOREN = ON
+#pragma config LVP = OFF
+#pragma config CPD = OFF
+#pragma config WRT = OFF
+#pragma config CP = OFF
